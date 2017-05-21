1a315374bccaeb8ee5738e20de3bffc8cf42d82a
Python3pkg <raliclo@gmail.com>
2017-05-21 02:06:24 -0700

    Convert to Python3

--- cookie_eater/__init__.py
+++ cookie_eater/__init__.py
@@ -4,7 +4,7 @@
 # Part of the CookieEater package.
 #
 # Copyright (c) 2017 - Chris Griffith - MIT License
-from __future__ import absolute_import
+
 from .firefox import *
 from .chrome import *
 from .base import *
